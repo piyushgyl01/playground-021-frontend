8e57391fb14e461bd97bfc3689a5d7d0e3831f6a
piyush.gyl <piyush2022ug@gmail.com>
2025-03-11 09:43:17 +0530

    chore: deployment chores

--- src/features/album/albumSlice.js
+++ src/features/album/albumSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://playground-021-backend.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
